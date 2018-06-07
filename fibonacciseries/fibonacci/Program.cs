7beee1a1929d54f176bc24d88859ae56980fc726
boppanavijaykumar <40016873+boppanavijaykumar@users.noreply.github.com>
2018-06-07 15:33:55 +0530

    Add files via upload

---
+++ fibonacciseries/fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 0, second = 1, third = 0,fiboinput=0;
+            Console.WriteLine("enter upto which number");
+            fiboinput = int.Parse(Console.ReadLine());
+            if (fiboinput == 0) 
+            for (int fibo=2;fibo<=fiboinput;fibo++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine(third);
+            }
+            Console.ReadLine();
+        }
+    }
+}
